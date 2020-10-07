436491c5098519626f010c21e00281497aeef158
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-07 00:21:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,16 +1,24 @@
 class Owner
-    attr_accessor 
-    attr_reader :name, :species
+  attr_accessor 
+  attr_reader :name, :species
+  
+  @@all = []
     
   
   def initialize(name, species = "human")
     @name = name
     @species = species
+    @@all << self
   end
   
   def say_species
     "I am a #{@species}."
   end
+  
+  def self.all
+    @@all
+  end
+  
   
   
 end
