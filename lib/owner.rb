b6bbc7f3acaebc03544752357ee9dae1175112d9
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-07 00:18:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,11 @@
 class Owner
+    attr_accessor :species
     attr_reader :name
+    
   
-  def initialize(name)
+  def initialize(name, species = "human")
     @name = name
+    @species = species
   end
   
   
