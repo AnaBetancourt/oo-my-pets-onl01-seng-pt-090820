d0d46c8f2740879427b41af531d890ec6ed649d3
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-07 00:28:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,18 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end 
   
   
   
